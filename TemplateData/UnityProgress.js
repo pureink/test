e2f7ee31da42d9d836249af03ee6e3e1a34f658b
badink <44235276+badink@users.noreply.github.com>
2020-10-15 19:39:08 +0800

    Add files via upload

---
+++ TemplateData/UnityProgress.js
@@ -0,0 +1,44 @@
+function UnityProgress(gameInstance, progress) {
+  if (!gameInstance.Module)
+    return;
+  if (!gameInstance.logo) {
+    gameInstance.logo = document.createElement("div");
+    gameInstance.logo.className = "logo " + gameInstance.Module.splashScreenStyle;
+    gameInstance.container.appendChild(gameInstance.logo);
+  }
+  if (!gameInstance.progress) {    
+    gameInstance.progress = document.createElement("div");
+    gameInstance.progress.className = "progress " + gameInstance.Module.splashScreenStyle;
+    gameInstance.progress.empty = document.createElement("div");
+    gameInstance.progress.empty.className = "empty";
+    gameInstance.progress.appendChild(gameInstance.progress.empty);
+    gameInstance.progress.full = document.createElement("div");
+    gameInstance.progress.full.className = "full";
+    gameInstance.progress.appendChild(gameInstance.progress.full);
+    gameInstance.container.appendChild(gameInstance.progress);
+  }
+  //创建一个html标签
+  if(!ele)
+  {
+	ele = document.createElement("div");
+    ele.className = "title";
+    gameInstance.progress.appendChild(ele);	
+  }
+   if(!tip)
+  {
+	tip = document.createElement("div");
+    tip.className = "tip";
+    gameInstance.progress.appendChild(tip);	
+	tip.innerHTML ="正在努力加载中···";
+  }
+  ele.innerHTML = (parseInt(100 * progress)) + "%";
+  gameInstance.progress.full.style.width = (100 * progress) + "%";
+  //gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  if (progress == 1)
+  {
+	gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
+	document.getElementById("version").hidden = true;
+  }
+}
+ var ele ; 
+ var tip ;
